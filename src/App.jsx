cdb4ca86b0d7487da7b4203b9126ed9a608e4845
Jayakodehode <jaya.kodehode@gmail.com>
2023-06-20 14:21:23 +0200

    imported router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
