ae2348c91e571ccdfa5c6fcf22b376ac1094647e
Jayakodehode <jaya.kodehode@gmail.com>
2023-06-21 12:52:32 +0200

    change base

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Landingpage } from "./routes/Landingpage/Landingpage";
+import { Home } from "./routes/Home/Home";
+import { About } from "./routes/About/About";
+import { Projects } from "./routes/Projects/Projects";
+import { Contact } from "./routes/Contact/Contact";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route></Route>
+          <Route path="/Jaya-React-Portfolio/" element={<Landingpage />}>
+            <Route index element={<Home />} />
+            <Route path="/Jaya-React-Portfolio/about" element={<About />} />
+            <Route
+              path="/Jaya-React-Portfolio/projects"
+              element={<Projects />}
+            />
+            <Route path="/Jaya-React-Portfolio/contact" element={<Contact />} />
+          </Route>
         </Routes>
       </Router>
     </>
